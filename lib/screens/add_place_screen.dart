826cba2bc8fd428c4fa46559442f2ea3fa4aba6e
Anil <anil.kumar@shellinfoglobal.com>
2024-10-14 19:02:43 +0530

    location input code added

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:favourite_places_app/providers/user_places.dart';
 import 'package:favourite_places_app/widgets/image_input.dart';
+import 'package:favourite_places_app/widgets/location_input.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -66,6 +67,10 @@ class _AddPlaceScreen extends ConsumerState<AddPlaceScreen> {
                   _selectedImage = image;
                 },
               ),
+              const SizedBox(
+                height: 16,
+              ),
+              const LocationInput(),
               const SizedBox(
                 height: 16,
               ),
