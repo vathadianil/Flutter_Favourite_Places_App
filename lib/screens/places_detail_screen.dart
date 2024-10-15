55b5cd57e48c33835b7b374b9db6023244e67bb0
Anil <anil.kumar@shellinfoglobal.com>
2024-10-15 10:43:43 +0530

    Showing map image on Detail screen

--- lib/screens/places_detail_screen.dart
+++ lib/screens/places_detail_screen.dart
@@ -1,9 +1,16 @@
 import 'package:favourite_places_app/models/place.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class PlacesDetailScreen extends StatelessWidget {
   const PlacesDetailScreen({super.key, required this.place});
   final Place place;
+
+  String get locationImage {
+    final lat = place.location.latitude;
+    final lng = place.location.longitude;
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$lat,$lng&key=${dotenv.env['API_KEY']}';
+  }
 
   @override
   Widget build(context) {
@@ -18,6 +25,40 @@ class PlacesDetailScreen extends StatelessWidget {
             fit: BoxFit.cover,
             width: double.infinity,
             height: double.infinity,
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 70,
+                  backgroundImage: NetworkImage(locationImage),
+                ),
+                Container(
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [Colors.transparent, Colors.black54],
+                    ),
+                  ),
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 24,
+                    vertical: 16,
+                  ),
+                  child: Text(
+                    place.location.address,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                        color:
+                            Theme.of(context).colorScheme.onPrimaryContainer),
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
--- lib/widgets/places_list.dart
+++ lib/widgets/places_list.dart
@@ -29,6 +29,11 @@ class PlacesList extends StatelessWidget {
             style: Theme.of(context).textTheme.titleMedium!.copyWith(
                 color: Theme.of(context).colorScheme.onPrimaryContainer),
           ),
+          subtitle: Text(
+            places[index].location.address,
+            style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                color: Theme.of(context).colorScheme.onPrimaryContainer),
+          ),
           onTap: () {
             Navigator.of(context).push(MaterialPageRoute(
                 builder: (ctx) => PlacesDetailScreen(place: places[index])));
